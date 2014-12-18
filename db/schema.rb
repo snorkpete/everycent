fded4f74574b4bf400e8d9d38b947568bd48bcef
Kion Stephen (work) <kion.stephen@ggil.biz>
2014-12-18 09:49:57 -0400

    add users model to backend api

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141218134311) do
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "password_hash"
+    t.string   "salt"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
 end
