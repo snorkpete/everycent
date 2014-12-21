d050e66b6e22f2dffd7ea9787c4282f4612a0435
Kion Stephen (laptop) <kion.stephen@gmail.com>
2014-12-20 21:05:26 -0400

    fix the authentication on the rails side

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  include DeviseTokenAuth::Concerns::User
+  
+  # do not require email confirmation for new users
+  before_create :skip_confirmation!
+  before_validation :generate_uid
+
+  protected
+  def generate_uid
+    self.provider = 'email'
+    self.uid = self.email if self.uid == ""
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: '/auth'
   resources :users, except: [:new, :edit]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
