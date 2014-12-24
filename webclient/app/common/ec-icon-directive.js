258606610ff9370c580a6f2d2a90d97f3eb4359d
Kion Stephen (laptop) <kion.stephen@gmail.com>
2014-12-24 13:22:01 -0400

    create new ec-icon directive for showing icons easily

---
+++ webclient/app/common/ec-icon-directive.js
@@ -0,0 +1,26 @@
+
+(function(){
+  'use strict';
+
+  angular
+    .module('everycent.common')
+    .directive('ecIcon', ecIcon);
+
+  function ecIcon(){
+    var directive = {
+      restrict:'E',
+      templateUrl: 'app/common/ec-icon-directive.html',
+      scope: {
+        type: '@'
+      },
+      controller: controller,
+      controllerAs: 'vm',
+      bindToController: true
+    }
+    return directive;
+
+    function controller(){
+      var vm = this;
+    }
+  }
+})();
