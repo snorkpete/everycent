e76326dd9fc9072c3809579e54448d0a9f8aa1b6
Kion Stephen (laptop) <kion.stephen@gmail.com>
2014-12-26 18:51:28 -0400

    annotate the existing models

--- test/models/bank_account_test.rb
+++ test/models/bank_account_test.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: bank_accounts
+#
+#  id              :integer          not null, primary key
+#  nickname        :string
+#  type            :string
+#  account_no      :string
+#  user_id         :integer
+#  institution_id  :integer
+#  opening_balance :integer
+#  current_balance :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'test_helper'
 
 class BankAccountTest < ActiveSupport::TestCase
