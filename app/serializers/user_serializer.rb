b110aaafc10f9b9eb1dbacd0640cd7c13bc52c2d
Kion Stephen (laptop) <kion.stephen@gmail.com>
2014-12-30 01:47:21 -0400

    Move the ec form field directive to load lookup options from the DB

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :nickname, :email
+  attributes :id, :name, :first_name, :last_name, :nickname, :email
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
