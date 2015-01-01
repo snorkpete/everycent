b02ab16f84dd3e83c86d96ef2eb8eb74b8c249b6
Kion Stephen (laptop) <kion.stephen@gmail.com>
2015-01-01 08:28:21 -0400

    update the model field annotations

--- test/models/allocation_category_test.rb
+++ test/models/allocation_category_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: allocation_categories
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  percentage :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class AllocationCategoryTest < ActiveSupport::TestCase
