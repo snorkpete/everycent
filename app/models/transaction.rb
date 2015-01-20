9bc1405ded09290d43c79e75ebccb19fdd8124c1
Kion Stephen (laptop) <kion.stephen@gmail.com>
2015-01-20 00:42:31 -0400

    add logic for saving multiple transactions at once

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,22 @@
 class Transaction < ActiveRecord::Base
+
+  def self.for_period(start_date, end_date, bank_account_id)
+    Transaction.where('transaction_date between ? and ?',start_date, end_date)
+               .where(bank_account_id: bank_account_id)
+  end
+
   def self.update_with_params(params)
+
+    # remove the existing transactions in the period
+    Transaction.for_period(params[:start_date], params[:end_date], params[:bank_account_id]).delete_all
+
+    # re-add the transactions that are being sent
+    params[:transactions].each do |transaction_params|
+      transaction_params[:bank_account_id] = params[:bank_account_id]
+      Transaction.create(transaction_params.except(:id))
+    end
+
+    #send back the fixed list of transactions
+    Transaction.for_period(params[:start_date], params[:end_date], params[:bank_account_id])
   end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#update_with_params' do
+    before :each do
+      @bank_account = create(:bank_account)
+      create(:transaction, description:'inside', transaction_date: '2015-01-10', bank_account_id: @bank_account.id)
+      create(:transaction, description:'outside', transaction_date: '2015-03-01', bank_account_id: @bank_account.id)
+
+      @params = {
+        start_date: '2015-01-10',
+        end_date: '2015-02-09',
+        bank_account_id: @bank_account.id,
+        transactions: [
+          { id: 5, description: 'first', transaction_date: '2015-01-15',
+            withdrawal_amount: 500, deposit_amount:0, payee_id: 3, allocation_id: 5 },
+
+          { id: 5, description: 'second', transaction_date: '2015-02-09',
+            withdrawal_amount: 0, deposit_amount: 1000, payee_id: 3, allocation_id: 5 }
+        ]
+      }
+    end
+
+    it 'removes existing transactions within the period and adds new transactions' do
+      @transactions = Transaction.update_with_params(@params)
+      expect(@transactions.size).to eq 2
+    end
+
+    it "ignores existing transactions that do not fit in the period" do
+      Transaction.update_with_params(@params)
+      expect(Transaction.all.size).to eq 3
+    end
+
+    it "creates the transactions with the indicated bank account" do
+      transactions = Transaction.update_with_params(@params)
+      transaction = transactions.last
+
+      expect(transaction.bank_account_id).to eq @bank_account.id
+    end
+
+    it "ignores transactions from other banks" do
+      create(:transaction, transaction_date: '2015-01-10', bank_account_id: 200)
+      transactions = Transaction.update_with_params(@params)
+
+      expect(Transaction.all.size).to eq 4
+      expect(transactions.size).to eq 2
+    end
+  end
 end
