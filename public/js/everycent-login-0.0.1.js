9a768dfdd60b7a430c1c6f360305ddfdfe446fa1
Kion Stephen (work) <kion.stephen@ggil.biz>
2015-01-23 15:35:08 -0400

    create a binding counter on the page for easy checking of binding count

--- public/js/everycent-login-0.0.1.js
+++ public/js/everycent-login-0.0.1.js
@@ -100,6 +100,57 @@
 
   angular
     .module('everycent.common')
+    .directive('ecBindingCount', ecBindingCount);
+
+  function ecBindingCount(){
+    var directive = {
+      restrict:'E',
+      template: '<button class="btn btn-info" ng-click="vm.updateBindingCount()">{{ vm.total }} bindings.</button>',
+      scope: {
+      },
+      controller: controller,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+    return directive;
+  }
+
+
+  controller.$inject = [];
+  function controller(){
+    /* jshint validthis: true */
+    var vm = this;
+    vm.updateBindingCount = updateBindingCount;
+
+    function getScopeList(rs) {
+        var scopeList = [];
+        function traverseScope(s) {
+            scopeList.push(s);
+            if (s.$$nextSibling) {
+                traverseScope(s.$$nextSibling);
+            }
+            if (s.$$childHead) {
+                traverseScope(s.$$childHead);
+            }
+        }
+        traverseScope(rs);
+        return scopeList;
+    }
+
+    function updateBindingCount(){
+      var scopes = getScopeList(angular.element(document.querySelectorAll("[ng-app]")).scope());
+      vm.total = _.uniq(_.flatten(scopes.map(function(s) { return s.$$watchers; }))).length;
+    }
+  }
+})();
+
+;
+
+(function(){
+  'use strict';
+
+  angular
+    .module('everycent.common')
     .directive('ecFormField', ecFormField);
 
   function ecFormField(){
