05c551c322b9ee31d209b18508b4b0494be5b100
Kion Stephen (laptop) <kion.stephen@gmail.com>
2015-01-25 14:15:27 -0400

    fix the invalid dateservice spec

--- webclient/test/unit/common/date-service.spec.js
+++ webclient/test/unit/common/date-service.spec.js
@@ -9,7 +9,7 @@ describe('DateService', function(){
 
   describe('#convertFromBankDateFormat', function(){
     it("swaps dates to 'yyyy-mm-dd' format", function(){
-      expect(DateService.convertFromBankDateFormat('01/05/2015')).toEqual('2015-01-05');
+      expect(DateService.convertFromBankDateFormat('01/05/2015')).toEqual('2015/01/05');
     });
     it("returns '' if not a valid date", function(){
       expect(DateService.convertFromBankDateFormat(undefined)).toEqual('');
