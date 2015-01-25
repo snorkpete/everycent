4fe184103a618f884361de0e102c78e9a45f5614
Kion Stephen (laptop) <kion.stephen@gmail.com>
2015-01-25 18:32:18 -0400

    update change_column migration to have USING statement

--- db/migrate/20150117143233_change_allocation_standing_order_to_boolean.rb
+++ db/migrate/20150117143233_change_allocation_standing_order_to_boolean.rb
@@ -1,6 +1,6 @@
 class ChangeAllocationStandingOrderToBoolean < ActiveRecord::Migration
   def self.up
-    change_column :allocations, :is_standing_order, :boolean
+    change_column :allocations, :is_standing_order, 'boolean USING CAST(is_standing_order AS boolean)'
   end
 
   def self.down
