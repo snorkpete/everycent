de2403c0f50ee146c0ff5f14993b1cf4422ad852
Kion Stephen <kion.stephen@gmail.com>
2015-03-30 18:51:02 -0400

    add expected_closing_balance to account balance display

--- app/serializers/account_balance_serializer.rb
+++ app/serializers/account_balance_serializer.rb
@@ -1,5 +1,5 @@
 class AccountBalanceSerializer < ActiveModel::Serializer
-  attributes :id, :name, :account_type, :account_category, :closing_balance, :current_balance
+  attributes :id, :name, :account_type, :account_category, :closing_balance, :expected_closing_balance, :current_balance
 
   has_one :institution
 end
