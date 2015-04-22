6afb70aa682e38046b43cfcbc52e60d2ea07b311
Kion Stephen (work) <kion.stephen@ggil.biz>
2015-04-22 15:56:57 -0400

    disable logging in test and development environments

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,6 +7,9 @@ Rails.application.configure do
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
+  # disable the logging to a file
+  config.logger = ActiveSupport::Logger.new(nil)
+
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
