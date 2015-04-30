3aef8228a19576a4b8816cabbeed221292e1898c
Kion Stephen (work) <kion.stephen@ggil.biz>
2015-04-30 18:44:37 -0400

    start building out the sink fund UI

--- app/serializers/sink_fund_serializer.rb
+++ app/serializers/sink_fund_serializer.rb
@@ -2,7 +2,7 @@
 #
 
 class SinkFundSerializer < ActiveModel::Serializer
-  attributes :id, :name, :account_type, :account_category, :account_no, :institution_id, :opening_balance, :closing_balance, :is_sink_fund
+  attributes :id, :name, :account_type, :account_category, :account_no, :institution_id, :opening_balance, :closing_balance, :current_balance, :sub_account_balance, :default_sub_account_amount, :is_sink_fund
 
   has_one :institution
   has_many :sub_accounts
