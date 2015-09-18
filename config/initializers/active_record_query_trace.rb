c692b4678c8693626dcefa928d895f1256c19488
Kion Stephen (work) <kion.stephen@ggil.biz>
2015-09-18 09:27:06 -0400

    add configuration for active_record_query_trace

---
+++ config/initializers/active_record_query_trace.rb
@@ -0,0 +1,13 @@
+# Be sure to restart your server when you modify this file.
+
+# activates the logging of where in the code a query initiates from
+
+ActiveRecordQueryTrace.enabled = true
+
+#app - includes only files in your app/ directory.
+#full - includes files in your app as well as rails.
+#rails - alternate output of full backtrace, useful for debugging gems.
+ActiveRecordQueryTrace.level = :app
+
+
+ActiveRecordQueryTrace.ignore_cached_queries = false # Default is false.
