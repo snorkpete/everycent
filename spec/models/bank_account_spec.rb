c82a1c65acb1f89b8f4c633478aa7bac98b473e6
Kion Stephen <kion.stephen@gmail.com>
2015-09-18 06:18:12 -0400

    add specs for credit card brought forward logic

--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -152,4 +152,27 @@ RSpec.describe BankAccount, :type => :model do
     end
 
   end
+
+
+  describe "credit cards" do
+    describe "#add_brought_forward_transactions" do
+      it "does nothing if the account is not a credit card"
+      it "adds new transactions for every transaction not paid in the credit period"
+      it "marks the new transactions with transaction date as one day after the end date"
+      it "marks the new transactions as brought_forward_status = 'added'"
+      it "makes the description of those new transactions = old description + (b/f)"
+      it "marks the not-paid transactions as 'paid'"
+      it "marks the not-paid transactions as brought_forward_status = 'brought_forward'"
+      it "creates a new transaction that is a sum of all 'added' transactions * -1"
+      it "labels the newly created transaction description as 'Balance B/F Adj Entry'"
+      it "marks the new transaction as brought_forward_status = 'added'"
+    end
+
+    describe "#remove_brought_forward_transactions" do
+      it "does nothing if the account is not a credit card"
+      it "changes brought_forward_status='brought_forward' transactions in the period back to 'unpaid'"
+      it "sets the brought_forward_status='brought_forward' transactions to nil"
+      it "removes transactions in the new period with brought_forward_status = 'added'"
+    end
+  end
 end
