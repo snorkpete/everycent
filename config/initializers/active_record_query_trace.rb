3c336f36533371418d1f3276a46da67c8de2e248
Kion Stephen (work) <kion.stephen@ggil.biz>
2015-09-21 14:16:51 -0400

    only configure active_record_query_trace in Development
    Not used in production or test because it slows down the logging

--- config/initializers/active_record_query_trace.rb
+++ config/initializers/active_record_query_trace.rb
@@ -1,13 +1,15 @@
 # Be sure to restart your server when you modify this file.
 
 # activates the logging of where in the code a query initiates from
+# currently only available in Development - slows down the logging
+if Rails.env.development?
+  ActiveRecordQueryTrace.enabled = true
 
-ActiveRecordQueryTrace.enabled = true
-
-#app - includes only files in your app/ directory.
-#full - includes files in your app as well as rails.
-#rails - alternate output of full backtrace, useful for debugging gems.
-ActiveRecordQueryTrace.level = :app
+  #app - includes only files in your app/ directory.
+  #full - includes files in your app as well as rails.
+  #rails - alternate output of full backtrace, useful for debugging gems.
+  ActiveRecordQueryTrace.level = :app
 
 
-ActiveRecordQueryTrace.ignore_cached_queries = false # Default is false.
+  ActiveRecordQueryTrace.ignore_cached_queries = false # Default is false.
+end
