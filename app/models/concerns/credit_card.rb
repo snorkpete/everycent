9c9df632f8f9a16b4dfb204e689e12f24be6f223
Kion Stephen <kion.stephen@gmail.com>
2015-09-21 20:18:13 -0400

    add comments to describe the CreditCard module

--- app/models/concerns/credit_card.rb
+++ app/models/concerns/credit_card.rb
@@ -17,6 +17,11 @@ module CreditCard
     is_credit_card
   end
 
+  # given the period, finds all unpaid transactions in the given period
+  # and creates new transactions in the next period for those unpaid transactions
+  # as well as a balancing transaction to offset the additions
+  # These new transactions are all labeled as '(b/f)'
+  # The original transactions that were copied are marked as 'paid'
   def add_brought_forward_transactions(start_date, end_date)
     return false unless credit_card?
 
@@ -32,10 +37,14 @@ module CreditCard
                                                           brought_forward_status: 'brought_forward'
   end
 
+  # returns the unpaid transactions that are eligible to be 
+  # copied into the new transaction period
   def transactions_to_copy(start_date, end_date)
     transactions.between(start_date, end_date).unpaid
   end
 
+  # builds the actual new transactions to be added 
+  # in the new period that mimic the original transactions to be copied
   def build_transactions_to_bring_forward(start_date, end_date)
     brought_forward_date = end_date.tomorrow
 
@@ -44,6 +53,9 @@ module CreditCard
     end
   end
 
+
+  # returns the adjustment transaction that balances the 
+  # transactions that will be copied to the new period
   def build_adjustment_transaction(start_date, end_date)
     withdrawals = transactions_to_copy(start_date, end_date).sum(:withdrawal_amount) * -1
     deposits = transactions_to_copy(start_date, end_date).sum(:deposit_amount) * -1
@@ -55,6 +67,10 @@ module CreditCard
     adjustment
   end
 
+  # reverses the addition of the brought forward transactions
+  # including the adjustment transaction
+  # Also changes the transactions that were copied to bring forward
+  # back to 'unpaid'
   def remove_brought_forward_transactions(start_date, end_date)
     # reset the original brought forward transactions back to unpaid
     transactions.between(start_date, end_date)
