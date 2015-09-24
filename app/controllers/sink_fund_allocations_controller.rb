f343ee1a5db19a6a2f0c59352cb51809e75278fb
Kion Stephen <kion.stephen@gmail.com>
2015-09-24 07:30:53 -0400

    sink fund allocation list should only include open ones

--- app/controllers/sink_fund_allocations_controller.rb
+++ app/controllers/sink_fund_allocations_controller.rb
@@ -2,7 +2,8 @@ class SinkFundAllocationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @sink_fund_allocations = SinkFundAllocation.where(bank_account_id: params[:bank_account_id])
+    @sink_fund_allocations = SinkFundAllocation
+                              .where(bank_account_id: params[:bank_account_id], status:'open')
     respond_with(@sink_fund_allocations, SinkFundAllocationSerializer)
   end
 end
