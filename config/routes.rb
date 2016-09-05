5e4000f431c01eebb93d66adc49680e0165d1792
Kion Stephen <kion.stephen@gmail.com>
2016-09-05 03:06:57 -0400

    add the basic authentication service

--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,59 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: '/auth'
   resources :users, except: [:new, :edit]
 
+
+  ############## V2 routes - duplicate of the V1 routes
+
+
+  scope 'v2' do
+    resources :transactions, except: [:new, :edit] do
+      collection do
+        put 'update_all'
+        get 'by_allocation'
+        get 'by_credit_card'
+        get 'last_update'
+      end
+    end
+    resources :payees, except: [:new, :edit]
+    resources :budgets, except: [:new, :edit] do
+      member do
+        put 'copy'
+        put 'close'
+      end
+
+      collection do
+        get 'current'
+        post 'reopen_last_budget'
+      end
+    end
+
+    resources :settings, only: [:index, :create]
+
+    resources :allocations, only: [:index]
+    resources :default_allocations, only: [:index] do
+      collection do
+        post 'retrieve'
+      end
+    end
+
+    resources :account_balances, only: [:index]
+    resources :sink_funds, only: [:index, :update]
+    resources :sink_fund_allocations, only: [:index]
+
+    resources :recurring_allocations, except: [:new, :edit]
+    resources :recurring_incomes, except: [:new, :edit]
+    resources :allocation_categories, except: [:new, :edit]
+    resources :bank_accounts, except: [:new, :edit]
+    resources :institutions, except: [:new, :edit]
+    resources :users, except: [:new, :edit]
+  end
+
+  namespace :v2 do
+    mount_devise_token_auth_for 'User', at: '/auth'
+  end
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
