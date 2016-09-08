2f90b62b51b4348c8ccf0e92dd5bf6065a647499
Kion Stephen <kion.stephen@gmail.com>
2016-09-08 00:11:12 -0400

    update the styling of the message display

--- webclientv2/src/app/shared/message-display.component.ts
+++ webclientv2/src/app/shared/message-display.component.ts
@@ -4,21 +4,32 @@ import {MessageService} from "../core/message.service";
 
 @Component({
   styles:[`
+    div.message-display{
+      font-size: 20px;
+      margin: 15px 25px;
+      padding: 10px;
+      border-radius: 5px;
+      color: white;
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); 
+    }
     .INFO{
-        background-color: lightblue;
+      background-color: #4f9c7c;
+      border: 3px solid #232d29;
     }
     .ERROR{
-        background-color: red;
+      background-color: #9c2545;
+      border: 3px solid #2d0b0c;
     }
     .WARNING{
-        background-color: orange;
+      background-color: #9c6514;
+      border: 3px solid #402909;
     }
     
   `],
   selector: 'ec-message-display',
   template: `
-    <div [class]="class$ | async">
-        Message: {{ messageText$ | async }}
+    <div class="message-display" [class]="class$ | async" *ngIf="messageText$ | async">
+        {{ messageText$ | async }}
     </div>
   `
 })
@@ -32,6 +43,6 @@ export class MessageDisplayComponent implements OnInit{
 
   ngOnInit(): void {
     this.messageText$ = this.messageService.message$.map(m => m.text);
-    this.class$ = this.messageService.message$.map(m => m.type);
+    this.class$ = this.messageService.message$.map(m => 'message-display ' + m.type);
   }
 }
