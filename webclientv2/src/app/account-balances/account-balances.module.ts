85f5cfc666817107198205bf65fc3a69cd28a5a1
Kion Stephen <kion.stephen@gmail.com>
2016-09-09 21:13:03 -0400

    create the account balances list

--- webclientv2/src/app/account-balances/account-balances.module.ts
+++ webclientv2/src/app/account-balances/account-balances.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from "@angular/core";
 import {SharedModule} from "../shared/shared.module";
 import {routing} from "./account-balances.routing";
 import {AccountBalancesComponent} from "./account-balances.component";
+import {AccountBalanceListComponent} from "./account-balance-list.component";
 import {AccountBalancesService} from "./account-balances.service";
 
 @NgModule({
   declarations:[
     AccountBalancesComponent,
+    AccountBalanceListComponent
   ],
   imports: [
     SharedModule,
