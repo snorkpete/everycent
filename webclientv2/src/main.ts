72f16bc926f5869707f703cf09c646e4da4cf508
Kion Stephen <kion.stephen@gmail.com>
2016-09-10 07:16:40 -0400

    add custom theming support - hack, but it works

--- webclientv2/src/main.ts
+++ webclientv2/src/main.ts
@@ -1,4 +1,6 @@
 import "./styles.scss";
+import "./material-overrides.scss";
+import "@angular2-material/core/overlay/overlay.css";
 
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import {AppModule} from "./app/app.module";
