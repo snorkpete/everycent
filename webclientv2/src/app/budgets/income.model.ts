24573ef560e88b5d721a4dd160030af53b66e849
Kion Stephen <kion.stephen@gmail.com>
2017-01-29 17:44:29 -0400

    incomplete work on the v2 income editor

--- webclientv2/src/app/budgets/income.model.ts
+++ webclientv2/src/app/budgets/income.model.ts
@@ -1,8 +1,18 @@
-export interface Income{
-
+export interface IncomeData{
+  id?: number,
   name: string,
   amount: number,
+  bank_account?: any,
   bank_account_id: number,
+  budget_id?: number,
   comment?: string,
   deleted?: boolean,
+}
+
+export class Income{
+  private incomeData: IncomeData;
+
+  set data(data: IncomeData){
+    this.data = data;
+  }
 }
