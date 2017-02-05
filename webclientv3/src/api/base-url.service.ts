be114155313f5289ddc73661092dac0122b421fe
Kion Stephen <kion.stephen@gmail.com>
2017-02-05 18:08:58 -0400

    set up the project to run from public directory in prod mode

---
+++ webclientv3/src/api/base-url.service.ts
@@ -0,0 +1,12 @@
+import {environment} from "../environments/environment";
+
+let BASE_URL = ''
+if (environment.production) {
+  BASE_URL = '';
+}
+else {
+  BASE_URL = 'http://localhost:3000';
+}
+
+export {BASE_URL};
+
--- webclientv3/src/app/app.module.ts
+++ webclientv3/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {ApiModule} from "../api/api.module";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
+    ApiModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
