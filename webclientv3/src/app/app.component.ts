355556fbca9ac715bcd8f37237d376afb7e539ab
Kion Stephen <kion.stephen@gmail.com>
2017-02-05 18:12:55 -0400

    fix the app component spec

--- webclientv3/src/app/app.component.ts
+++ webclientv3/src/app/app.component.ts
@@ -7,7 +7,9 @@ import {ApiGateway} from '../api/api-gateway.service';
   styles: [`
   `],
   template: `
-    Hello from trinidad {{ title }}
+    <h1>
+      Hello from trinidad {{ title }}
+    </h1>
   `,
 })
 export class AppComponent implements OnInit{
