66013d1351429d2985cbb2a00c1c807d6c992ab8
Kion Stephen <kion.stephen@gmail.com>
2017-02-10 06:19:43 -0400

    check that the api gateway driven requests are accurate

--- webclientv3/src/api/api-gateway.service.ts
+++ webclientv3/src/api/api-gateway.service.ts
@@ -1,19 +1,19 @@
 import {Injectable} from "@angular/core";
 import {Http} from "@angular/http";
 import {BASE_URL} from "./base-url.service";
-import {Observable} from 'rxjs';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class ApiGateway {
 
   private BASE_URL = BASE_URL;
   constructor(
-    private http: Http
+    public http: Http
   ) {}
 
-  get(): Observable<any> {
+  get(url: string, params?: any): Observable<any> {
 
-    return this.http.get(this.BASE_URL + '/account_balances')
+    return this.http.get(this.BASE_URL + '/' + url)
                .map(res => res.json());
   }
 }
