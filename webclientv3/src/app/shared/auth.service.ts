3c834f5a4b13473cc860a96ff234f52b702fb2a9
Kion Stephen <kion.stephen@gmail.com>
2017-02-18 18:29:23 -0400

    implement the login functionality

---
+++ webclientv3/src/app/shared/auth.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import {ApiGateway} from '../../api/api-gateway.service';
+import 'rxjs/add/operator/toPromise';
+import {Observable} from 'rxjs/Observable';
+
+@Injectable()
+export class AuthService {
+
+  constructor(
+    private apiGateway: ApiGateway
+  ) { }
+
+  logIn(email: string, password: string): Promise<any> {
+    return this.apiGateway
+            .postWithoutAuthentication('/login', {email, password})
+            .do(userCredentials => this.saveCredentials(userCredentials))
+            // On any error, clear the credentials and rethrow the error
+            .catch(error => {
+              this.clearCredentials();
+              return Observable.throw(error);
+            })
+            .toPromise();
+  }
+
+  private saveCredentials(userCredentials: any): void {
+    localStorage.setItem('access-token', userCredentials['access-token']);
+    localStorage.setItem('client', userCredentials['client']);
+    localStorage.setItem('expiry', userCredentials['expiry']);
+    localStorage.setItem('token-type', userCredentials['token-type']);
+    localStorage.setItem('uid', userCredentials['uid']);
+  }
+
+  private clearCredentials(): void {
+    localStorage.removeItem('access-token');
+    localStorage.removeItem('client');
+    localStorage.removeItem('expiry');
+    localStorage.removeItem('token-type');
+    localStorage.removeItem('uid');
+  }
+}
+
