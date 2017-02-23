dd2b95617445d8ac087503871c4eca961bc3a229
Kion Stephen <kion.stephen@gmail.com>
2017-02-23 19:56:45 -0400

    implement the message display component

--- webclientv3/src/app/shared/message-display/message-display.component.ts
+++ webclientv3/src/app/shared/message-display/message-display.component.ts
@@ -1,17 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import {MessageService, MessageType} from '../../message-display/message.service';
 
 @Component({
   selector: 'ec-message-display',
+  styles: [`
+    div.message-display{
+        text-align: center;
+        margin: 10px 15px;
+        padding: 5px;
+        font-family: Roboto,"Helvetica Neue",sans-serif;
+        font-weight: 400;
+        font-size: 1.1em;
+        border-radius: 5px;
+    }
+
+    div.error {
+        color: white;
+        background-color: #ffb300;
+        border: 2px solid #886b27;
+    }
+    div.info {
+        color: white;
+        background-color: #8084ec;
+        border: 2px solid #21236f;
+    }
+  `],
   template: `
-    <p>
-      message-display Works!
-    </p>
-  `,
-  styles: []
+    <div *ngIf="messageService.getMessage()"
+         class="message-display"
+         [class.info]="messageService.isInfo()"
+         [class.error]="messageService.isError()">
+        {{ messageService.getMessage() }}
+    </div>
+  `
 })
 export class MessageDisplayComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public messageService: MessageService
+  ) { }
 
   ngOnInit() {
   }
