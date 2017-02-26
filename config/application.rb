d9194e2c62c87243f1866b5625a8d5c1e3381060
Kion Stephen <kion.stephen@gmail.com>
2017-02-26 19:44:32 -0400

    fix the cors configuration to expose the authentication headers

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,10 @@ module Everycent
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*',
+                 :headers => :any,
+                 :methods => [:get, :post, :options],
+                 :expose => ['access-token', 'client', 'expiry', 'token-type', 'uid']
       end
     end
   end
