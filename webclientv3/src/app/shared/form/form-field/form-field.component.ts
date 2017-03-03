8f5bc18a5b2662efae865b319d8d274d7b4c744a
Kion Stephen <kion.stephen@gmail.com>
2017-03-03 18:58:16 -0400

    create default scaffolding for form field

---
+++ webclientv3/src/app/shared/form/form-field/form-field.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ec-form-field',
+  template: `
+    <p>
+      form-field Works!
+    </p>
+  `,
+  styles: []
+})
+export class FormFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
