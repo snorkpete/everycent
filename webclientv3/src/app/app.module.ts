e7744240c5fe916279de7a161d1cf1dc6317ef99
Kion Stephen <kion.stephen@gmail.com>
2017-03-03 18:21:53 -0400

    code formatting cleanup

--- webclientv3/src/app/app.module.ts
+++ webclientv3/src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import {ApiModule} from "../api/api.module";
+import {AppComponent} from './app.component';
+import {ApiModule} from '../api/api.module';
 import {SharedModule} from './shared/shared.module';
-import {RouterModule} from '@angular/router';
-import {MaterialModule} from '@angular/material';
-import {AppRoutingModule} from './app-routing.module';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './home/home.component';
 import {SinkFundsModule} from './sink-funds/sink-funds.module';
 
 @NgModule({
@@ -28,7 +25,7 @@ import {SinkFundsModule} from './sink-funds/sink-funds.module';
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
