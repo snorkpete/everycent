902396820629dc439649211e4edb833d9a4c1a15
Kion Stephen <kion.stephen@gmail.com>
2017-03-05 13:42:29 -0400

    implement the form field
    This is a field that uses an attribute to select
    between different field types

--- webclientv3/src/app/shared/form/form-field/form-field.component.ts
+++ webclientv3/src/app/shared/form/form-field/form-field.component.ts
@@ -1,19 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, forwardRef, Input, OnInit} from '@angular/core';
+import {ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR} from '@angular/forms';
+
+let FormType = {
+  TEXT: 'text',
+  DATE: 'date',
+  MONEY: 'money',
+  NONE: 'none',
+};
+
+export {FormType};
 
 @Component({
   selector: 'ec-form-field',
+  styles: [`
+  `],
   template: `
-    <p>
-      form-field Works!
-    </p>
+    <ec-text-field *ngIf="type==FormType.TEXT" [editMode]="editMode" [(ngModel)]="value" [formControl]="control"></ec-text-field>
+    <ec-date-field *ngIf="type==FormType.DATE" [editMode]="editMode" [(ngModel)]="value" [formControl]="control"></ec-date-field>
+    <ec-money-field *ngIf="type==FormType.MONEY" [editMode]="editMode" [(ngModel)]="value" [formControl]="control"></ec-money-field>
   `,
-  styles: []
+  providers: [
+    { provide: NG_VALUE_ACCESSOR, multi: true, useExisting: forwardRef(() => FormFieldComponent )}
+  ]
 })
-export class FormFieldComponent implements OnInit {
+export class FormFieldComponent implements OnInit, ControlValueAccessor {
+
+  FormType = FormType;
+
+  @Input() value: string | number | Date;
+  @Input() editMode: boolean;
+  @Input() type: string;
+
+  control = new FormControl();
+
+  private onChange: Function = (_: any) => {};
+  private onTouch: Function = () => {};
+
 
   constructor() { }
 
   ngOnInit() {
+    this.control.valueChanges.subscribe((v: string) => {
+      this.onChange(v);
+    });
+  }
+
+  writeValue(newValue: string): void {
+    this.value = newValue;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
   }
 
 }
