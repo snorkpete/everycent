2cb51b7c6375c9d382266ce76a053e488293603a
Kion Stephen <kion.stephen@gmail.com>
2017-03-25 04:11:03 -0400

    update configuration to angular 4

--- webclientv3/karma.conf.js
+++ webclientv3/karma.conf.js
@@ -4,37 +4,38 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', 'angular-cli'],
+    frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-phantomjs-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
       require('karma-spec-reporter'),
-      require('karma-remap-istanbul'),
-      require('angular-cli/plugins/karma')
+      require('@angular/cli/plugins/karma')
     ],
+    client:{
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
     files: [
       { pattern: './src/test.ts', watched: false }
     ],
     preprocessors: {
-      './src/test.ts': ['angular-cli']
+      './src/test.ts': ['@angular/cli']
     },
     mime: {
       'text/x-typescript': ['ts','tsx']
     },
-    remapIstanbulReporter: {
-      reports: {
-        html: 'coverage',
-        lcovonly: './coverage/coverage.lcov'
-      }
+    coverageIstanbulReporter: {
+      reports: [ 'html', 'lcovonly' ],
+      fixWebpackSourcePaths: true
     },
     angularCli: {
-      config: './angular-cli.json',
       environment: 'dev'
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['dots', 'karma-remap-istanbul']
-              : ['spec'], // Use the spec reporter
+              ? ['dots', 'coverage-istanbul']
+              : ['spec', 'kjhtml' ], // Use the spec reporter
               //: ['dots'],   // use the dots reporter
     port: 9876,
     colors: true,
