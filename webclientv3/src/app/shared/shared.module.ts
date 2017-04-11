ee289420c1078f8397ce208fd2ec7271694d6d03
Kion Stephen <kion.stephen@gmail.com>
2017-04-10 21:13:11 -0400

    add display of the 'show transactions' icon on sink funds

--- webclientv3/src/app/shared/shared.module.ts
+++ webclientv3/src/app/shared/shared.module.ts
@@ -68,6 +68,7 @@ import { HighlightDeletedDirective } from './highlight-deleted.directive';
     MessageDisplayComponent,
     MenuComponent,
 
+    EcIconComponent,
     TextFieldComponent,
     DateFieldComponent,
     MoneyFieldComponent,
