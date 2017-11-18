aed7effaa2c7cb6f8303531927aa57cfbb398ad7
Kion Stephen <kion.stephen@gmail.com>
2017-11-18 19:47:13 +0100

    add commas to money display

--- webclientv3/src/app/util/cents-to-dollars.ts
+++ webclientv3/src/app/util/cents-to-dollars.ts
@@ -8,7 +8,9 @@ let centsToDollars = (centsValue: number): string => {
   }
 
   let dollarValue = centsValue / 100;
-  return dollarValue.toFixed(2);
+  return dollarValue.toFixed(2)
+                    .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+
 };
 
 export { centsToDollars };
