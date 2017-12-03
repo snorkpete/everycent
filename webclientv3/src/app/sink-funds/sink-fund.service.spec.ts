c6bfd794ff81176ac4b7d8ba37c440a01a540bca
Kion Stephen <kion.stephen@gmail.com>
2017-12-03 03:35:00 +0100

    more failing test fixes

--- webclientv3/src/app/sink-funds/sink-fund.service.spec.ts
+++ webclientv3/src/app/sink-funds/sink-fund.service.spec.ts
@@ -31,7 +31,7 @@ describe('SinkFundService', () => {
     expect(sinkFundService).toBeTruthy();
   });
 
-  xdescribe('#getCurrent()', () => {
+  describe('#getCurrent()', () => {
 
     it('exists', async(() => {
       expect(sinkFundService.getCurrent).toBeDefined();
@@ -41,12 +41,13 @@ describe('SinkFundService', () => {
       let response = SampleSinkFundData;
       let spy = spyOn(apiGateway, 'get').and.returnValue(Observable.of(response));
 
-      sinkFundService.getCurrent().subscribe(sinkFund => {
+      sinkFundService.refreshSinkFund();
 
-          expect(spy.calls.any()).toBeTruthy('calls the apiGateway');
-          expect(spy.calls.mostRecent().args[0]).toEqual('/sink_funds/current');
-          expect(sinkFund).toEqual(SampleSinkFundData);
-        });
+      expect(spy.calls.any()).toBeTruthy('calls the apiGateway');
+      expect(spy.calls.mostRecent().args[0]).toEqual('/sink_funds/current');
+      sinkFundService.getCurrent().subscribe(sinkFund => {
+        expect(sinkFund).toEqual(SampleSinkFundData);
+      });
     }));
   });
 
