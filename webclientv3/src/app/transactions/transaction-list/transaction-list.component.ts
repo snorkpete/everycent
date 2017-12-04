e303bb219eac0bafa8ed6e37aa4e8f9957b1b074
Kion Stephen <kion.stephen@gmail.com>
2017-12-04 20:34:23 +0100

    implement the transaction list as a material table

--- webclientv3/src/app/transactions/transaction-list/transaction-list.component.ts
+++ webclientv3/src/app/transactions/transaction-list/transaction-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {TransactionData} from "../transaction-data.model";
 import {MatTableDataSource} from "@angular/material";
 import {BehaviorSubject} from "rxjs/BehaviorSubject";
@@ -6,81 +6,111 @@ import {BehaviorSubject} from "rxjs/BehaviorSubject";
 @Component({
   selector: 'ec-transaction-list',
   styles: [`
+
+    ec-money-field {
+      text-align: end;
+    }
+    .table-container {
+      max-height: 400px;
+    }
     .table-container {
       position: relative;
       margin-top:50px;
     }
-    .table-container .mat-table {
+    .table-container mat-table {
       max-height: calc(100vh - 200px);
       overflow-y: auto;
     }
-    .table-container .mat-table .mat-header-row {
+    .table-container .mat-table mat-header-row {
       position: absolute;
       top: -50px;
       left: 0px;
       right: 18px;
       background: #fff;
-    } 
+    }
   `],
   template: `
-    <div class="table-container">
-    <mat-table #table [dataSource]="dataSource">
+    <mat-card>
+        <mat-card-title>Transactions</mat-card-title>
+        <mat-card-content>
+          <div class="table-container">
+          <mat-table #table [dataSource]="dataSource">
 
-      <!-- Select Column -->
-      <ng-container matColumnDef="select">
-        <mat-header-cell *matHeaderCellDef fxFlex="0 0 10"> </mat-header-cell>
-        <mat-cell *matCellDef="let transaction" fxFlex="0 0 10"> {{transaction.id}} </mat-cell>
-      </ng-container>
+            <!-- Select Column -->
+            <ng-container matColumnDef="select">
+              <mat-header-cell *matHeaderCellDef fxFlex="0 0 3"> <mat-checkbox></mat-checkbox> </mat-header-cell>
+              <mat-cell *matCellDef="let transaction" fxFlex="0 0 3">
+                <mat-checkbox></mat-checkbox>
+              </mat-cell>
+            </ng-container>
 
-      <!-- Date Column -->
-      <ng-container matColumnDef="transactionDate">
-        <mat-header-cell *matHeaderCellDef fxFlex="0 0 10">Date</mat-header-cell>
-        <mat-cell *matCellDef="let transaction" fxFlex="0 0 10"> {{transaction.transaction_date}} </mat-cell>
-      </ng-container>
+            <!-- Date Column -->
+            <ng-container matColumnDef="transactionDate">
+              <mat-header-cell *matHeaderCellDef fxFlex="0 0 10">Date</mat-header-cell>
+              <mat-cell *matCellDef="let transaction" fxFlex="0 0 10">
+                <ec-date-field [editMode]="isEditMode" [(ngModel)]="transaction.transaction_date"></ec-date-field>
+              </mat-cell>
+            </ng-container>
 
-      <!-- Description Column -->
-      <ng-container matColumnDef="description">
-        <mat-header-cell *matHeaderCellDef fxFlex="0 0 30">Description</mat-header-cell>
-        <mat-cell *matCellDef="let transaction" fxFlex="0 0 30"> {{transaction.description}} </mat-cell>
-      </ng-container>
+            <!-- Description Column -->
+            <ng-container matColumnDef="description">
+              <mat-header-cell *matHeaderCellDef fxFlex="0 0 30">Description</mat-header-cell>
+              <mat-cell *matCellDef="let transaction" fxFlex="0 0 30">
+                <ec-text-field [editMode]="isEditMode" [(ngModel)]="transaction.description"></ec-text-field>
+              </mat-cell>
+            </ng-container>
 
-      <!-- Allocation Column -->
-      <ng-container matColumnDef="allocation">
-        <mat-header-cell *matHeaderCellDef fxFlex="0 0 20">Allocation</mat-header-cell>
-        <mat-cell *matCellDef="let transaction" fxFlex="0 0 20"> &nbsp; {{transaction.allocation}} </mat-cell>
-      </ng-container>
+            <!-- Allocation Column -->
+            <ng-container matColumnDef="allocation">
+              <mat-header-cell *matHeaderCellDef fxFlex="0 0 20">Allocation</mat-header-cell>
+              <mat-cell *matCellDef="let transaction" fxFlex="0 0 20">
+                <ec-text-field [editMode]="isEditMode" [value]="transaction.allocation?.name"></ec-text-field>
+              </mat-cell>
+            </ng-container>
 
-      <!-- Withdrawn Column -->
-      <ng-container matColumnDef="withdrawn">
-        <mat-header-cell *matHeaderCellDef fxFlex="0 0 10">Withdrawn</mat-header-cell>
-        <mat-cell *matCellDef="let transaction" fxFlex="0 0 10"> {{transaction.withdrawal_amount}} </mat-cell>
-      </ng-container>
+            <!-- Withdrawn Column -->
+            <ng-container matColumnDef="withdrawn">
+              <mat-header-cell *matHeaderCellDef fxFlex="0 0 10">Withdrawn</mat-header-cell>
+              <mat-cell *matCellDef="let transaction" fxFlex="0 0 10">
+                <ec-money-field [editMode]="isEditMode" [(ngModel)]="transaction.withdrawal_amount"></ec-money-field>
+              </mat-cell>
+            </ng-container>
 
-      <!-- Deposit Column -->
-      <ng-container matColumnDef="deposit">
-        <mat-header-cell *matHeaderCellDef fxFlex="0 0 10">Deposit</mat-header-cell>
-        <mat-cell *matCellDef="let transaction" fxFlex="0 0 10"> {{transaction.deposit_amount}} </mat-cell>
-      </ng-container>
+            <!-- Deposit Column -->
+            <ng-container matColumnDef="deposit">
+              <mat-header-cell *matHeaderCellDef fxFlex="0 0 10">Deposit</mat-header-cell>
+              <mat-cell *matCellDef="let transaction" fxFlex="0 0 10">
+                <ec-money-field [editMode]="isEditMode" [(ngModel)]="transaction.deposit_amount"></ec-money-field>
+              </mat-cell>
+            </ng-container>
 
-      <!-- Paid Column -->
-      <ng-container matColumnDef="paid">
-        <mat-header-cell *matHeaderCellDef fxFlex="0 0 10">Paid?</mat-header-cell>
-        <mat-cell *matCellDef="let transaction" fxFlex="0 0 10"> {{transaction.status == 'paid' ? 'Yes' : 'No' }} </mat-cell>
-      </ng-container>
+            <!-- Paid Column -->
+            <ng-container matColumnDef="paid">
+              <mat-header-cell *matHeaderCellDef fxFlex="0 0 10">Paid?</mat-header-cell>
+              <mat-cell *matCellDef="let transaction" fxFlex="0 0 10">
+                {{transaction.status == 'paid' ? 'Yes' : 'No' }}
+              </mat-cell>
+            </ng-container>
 
-      <!-- Action Column -->
-      <ng-container matColumnDef="action">
-        <mat-header-cell *matHeaderCellDef fxFlex="0 0 10"></mat-header-cell>
-        <mat-cell *matCellDef="let transaction" fxFlex="0 0 10"> 
-          <ec-delete-button [item]="transaction"></ec-delete-button>
-        </mat-cell>
-      </ng-container>
+            <!-- Action Column -->
+            <ng-container matColumnDef="action">
+              <mat-header-cell *matHeaderCellDef fxFlex="0 0 10"></mat-header-cell>
+              <mat-cell *matCellDef="let transaction" fxFlex="0 0 10">
+                <ec-delete-button [item]="transaction"></ec-delete-button>
+              </mat-cell>
+            </ng-container>
 
-      <mat-header-row *matHeaderRowDef="displayedColumns"></mat-header-row>
-      <mat-row *matRowDef="let row; columns: displayedColumns;"></mat-row>
-    </mat-table> 
-    </div>
-  `
+            <mat-header-row *matHeaderRowDef="displayedColumns"></mat-header-row>
+            <mat-row *matRowDef="let row; columns: displayedColumns;"></mat-row>
+          </mat-table>
+          </div>
+      </mat-card-content>
+      <mat-card-actions>
+        <ec-edit-actions [editMode]="isEditMode" (save)="save()" (cancel)="cancel()"></ec-edit-actions>
+      </mat-card-actions>
+    </mat-card>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TransactionListComponent implements OnInit, OnChanges {
 
@@ -89,7 +119,10 @@ export class TransactionListComponent implements OnInit, OnChanges {
   dataSource = new MatTableDataSource<TransactionData>();
   data = this.dataSource.connect();
   displayedColumns = ['select', 'transactionDate', 'description',
-                      'allocation', 'withdrawn', 'deposit','paid', 'action'];
+                      'allocation', 'withdrawn', 'deposit', 'paid', 'action'];
+
+  @Input()
+  isEditMode = false;
 
   constructor() { }
 
@@ -103,4 +136,11 @@ export class TransactionListComponent implements OnInit, OnChanges {
     }
   }
 
+  save(): void {
+
+  }
+
+  cancel(): void {
+
+  }
 }
