2e37a20e09f4fbd47dfcb121f5c9f8523b56edd7
Kion Stephen <kion.stephen@gmail.com>
2017-12-04 20:34:23 +0100

    implement the transaction list as a material table

--- webclientv3/src/app/transactions/transaction-list/transaction-list.component.spec.ts
+++ webclientv3/src/app/transactions/transaction-list/transaction-list.component.spec.ts
@@ -2,10 +2,13 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TransactionListComponent } from './transaction-list.component';
 import {SharedModule} from "../../shared/shared.module";
+import {DebugElement} from "@angular/core";
+import {By} from "@angular/platform-browser";
 
 describe('TransactionListComponent', () => {
   let component: TransactionListComponent;
   let fixture: ComponentFixture<TransactionListComponent>;
+  let de: DebugElement<TransactionListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -20,10 +23,16 @@ describe('TransactionListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TransactionListComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('has an edit-actions component', () => {
+    let editActionsEl = de.query(By.css('ec-edit-actions'));
+    expect(editActionsEl).toBeTruthy();
   });
 });
