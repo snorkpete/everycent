9384e823cf152b19bda15d05438065c9a95d48f3
Kion Stephen <kion.stephen@gmail.com>
2017-12-04 20:33:47 +0100

    fix dollarsToCents to handle dollar amounts with commas

--- webclientv3/src/app/util/dollars-to-cents.ts
+++ webclientv3/src/app/util/dollars-to-cents.ts
@@ -1,6 +1,10 @@
 
 let dollarsToCents = (dollarValue: number | string): number => {
-  let numericDollarValue = Number(dollarValue);
+  let dollarValueToConvert = dollarValue;
+  if(dollarValue && dollarValue.replace) {
+    dollarValueToConvert = dollarValue.replace(/,/g, '');
+  }
+  let numericDollarValue = Number(dollarValueToConvert);
   if (isNaN(numericDollarValue)) {
     return 0;
   }
