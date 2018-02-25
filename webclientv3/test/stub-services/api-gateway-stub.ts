83312960eb9988ccc5ad90b498cd5bd728fcc45d
Kion Stephen <kion.stephen@gmail.com>
2018-02-25 13:59:39 +0100

    provide better stubs on the ApiGatewayStub

--- webclientv3/test/stub-services/api-gateway-stub.ts
+++ webclientv3/test/stub-services/api-gateway-stub.ts
@@ -1,9 +1,9 @@
 import {Observable} from "rxjs/Observable";
 
 let ApiGatewayStub = {
-  get: (url: string, params: any) => { Observable.of(null); },
-  post: (url: string, params: any, data?: any) => {},
-  put: (url: string, params: any, data?: any) => {}
+  get: (url: string, params: any) => Observable.of([]),
+  post: (url: string, params: any, data?: any) => Observable.of([]),
+  put: (url: string, params: any, data?: any) => Observable.of([])
 }
 
 export {ApiGatewayStub};
