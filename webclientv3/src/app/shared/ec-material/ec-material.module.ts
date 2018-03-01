5df81b3b3276aad40abe7a8076c8cbe0f1c2a7a8
Kion Stephen <kion.stephen@gmail.com>
2018-03-01 23:50:44 +0100

    clean up Material Checkbox import

--- webclientv3/src/app/shared/ec-material/ec-material.module.ts
+++ webclientv3/src/app/shared/ec-material/ec-material.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import {FlexLayoutModule} from "@angular/flex-layout";
 import {MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card";
+import {MatCheckboxModule} from "@angular/material/checkbox";
 import {MatDialogModule} from "@angular/material/dialog";
 import {MatIconModule} from "@angular/material/icon";
 import {MatInputModule} from "@angular/material/input";
@@ -14,7 +15,6 @@ import {MatSlideToggleModule} from "@angular/material/slide-toggle";
 import {MatSnackBarModule} from "@angular/material/snack-bar";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatTableModule} from "@angular/material/table";
-import {MatCheckboxModule} from "@angular/material";
 
 
 @NgModule({
