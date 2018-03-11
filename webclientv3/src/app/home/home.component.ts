1d865222534b458c51eee46c83263e4565269889
Kion Stephen <kion.stephen@gmail.com>
2018-03-11 14:11:18 +0100

    Add a changelog

--- webclientv3/src/app/home/home.component.ts
+++ webclientv3/src/app/home/home.component.ts
@@ -4,7 +4,26 @@ import {MainToolbarService} from '../shared/main-toolbar/main-toolbar.service';
 @Component({
   selector: 'ec-home',
   template: `
-    Home
+    <h1>Recent Updates and Fixes</h1>
+    <ul>
+      <li>Implement the Future Budgets view</li>
+      <li>Prevent adding transactions to future budgets</li>
+    </ul>
+
+    <h3>Still outstanding</h3>
+    <ul>
+      <li>
+        Fix issue where the transaction summary shows the wrong amount after saving. <br/>
+        The current workaround is to click 'Refresh' - this properly updates the transaction summary
+      </li>
+      <li>
+        The Future Budgets view does not have an accurate total of allocations. Only a dummy value is supplied currently.
+      </li>
+      <li>
+        Move the selection of the import format to part of the configuration of the account.
+        By doing this, selecting an account will select the appropriate import format properly.
+      </li>
+    </ul>
   `,
   styles: []
 })
