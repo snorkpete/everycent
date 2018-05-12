486c727732835b853b2ca3842a452d7eb87b5f73
Kion Stephen <kion.stephen@gmail.com>
2018-05-12 07:22:44 +0200

    change the main app layout

--- webclientv3/src/app/app.component.ts
+++ webclientv3/src/app/app.component.ts
@@ -6,25 +6,39 @@ import {Component, OnInit} from '@angular/core';
     :host {
         display: flex;
         flex: 1;
-    }
-  `],
+      }
+      mat-sidenav-container {
+        flex: 1;
+      }
+      ec-main-toolbar {
+        position: fixed;
+        top: 0px;
+        width: 100%;
+        z-index: 10000;
+      }
+      div.holding-container {
+        margin-top: 35px;
+      }
+  `
+  ],
   template: `
-    <div fxLayout="column" fxFlex>
-       <ec-main-toolbar (openMenu)="sideNav.toggle()"> </ec-main-toolbar>
+       <mat-sidenav-container>
 
-         <mat-sidenav-container fxFlex class="main">
+         <mat-sidenav #sideNav [opened]="true" mode="side" [fixedInViewport]="true">
+           <ec-menu (menuSelect)="onMenuSelect()"></ec-menu>
+         </mat-sidenav>
 
-           <mat-sidenav #sideNav [opened]="true" mode="side">
-             <ec-menu (menuSelect)="onMenuSelect()"></ec-menu>
-           </mat-sidenav>
+         <mat-sidenav-content>
+           <ec-main-toolbar (openMenu)="sideNav.toggle()"> </ec-main-toolbar>
+           <div class="holding-container">
+             <ec-loading-indicator></ec-loading-indicator>
+             <ec-message-display></ec-message-display>
+             <router-outlet></router-outlet>
+           </div>
+         </mat-sidenav-content>
 
-           <ec-loading-indicator></ec-loading-indicator>
-           <ec-message-display></ec-message-display>
-           <router-outlet></router-outlet>
-
-         </mat-sidenav-container>
-     </div>
-  `,
+       </mat-sidenav-container>
+  `
 })
 export class AppComponent implements OnInit {
 
