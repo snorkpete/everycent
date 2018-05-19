5fd9661aebfd0aa34febb322d57b093e36504cda
Kion Stephen <kion.stephen@gmail.com>
2018-05-19 12:38:40 +0200

    Prettier reformat

--- webclientv3/src/app/budgets/budgets/budgets.component.ts
+++ webclientv3/src/app/budgets/budgets/budgets.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import {MatDialog} from "@angular/material";
-import {ActivatedRoute, Router} from "@angular/router";
-import {MessageService} from "../../message-display/message.service";
-import {ConfirmationService} from "../../shared/confirmation.service";
-import {ConfirmationComponent} from "../../shared/confirmation/confirmation.component";
-import {MainToolbarService} from "../../shared/main-toolbar/main-toolbar.service";
-import {BudgetData} from "../budget.model";
-import {BudgetService} from "../budget.service";
+import { Component, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material";
+import { ActivatedRoute, Router } from "@angular/router";
+import { MessageService } from "../../message-display/message.service";
+import { ConfirmationService } from "../../shared/confirmation.service";
+import { ConfirmationComponent } from "../../shared/confirmation/confirmation.component";
+import { MainToolbarService } from "../../shared/main-toolbar/main-toolbar.service";
+import { AddBudgetComponent } from "../add-budget/add-budget.component";
+import { BudgetData } from "../budget.model";
+import { BudgetService } from "../budget.service";
 
 @Component({
-  selector: 'ec-budgets',
+  selector: "ec-budgets",
   template: `
     <mat-card class="main">
       <mat-card-title>Budgets</mat-card-title>
@@ -42,12 +43,14 @@ export class BudgetsComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.toolbar.setHeading('Budgets');
+    this.toolbar.setHeading("Budgets");
     this.refresh();
   }
 
   refresh() {
-    this.budgetService.getBudgets().subscribe(budgets => this.budgets = budgets );
+    this.budgetService
+      .getBudgets()
+      .subscribe(budgets => (this.budgets = budgets));
   }
 
   addNewBudget() {
@@ -56,18 +59,19 @@ export class BudgetsComponent implements OnInit {
   }
 
   reopenLastBudget() {
-    this.confirmation.ask({
-      title: 'Reopen Last Budget',
-      question: 'Are you sure you want to open the last closed budget?',
-      emitNegativeAnswers: false
-    })
-    .subscribe(() => {
-      this.messageService.setMessage("Reopening last budget...");
-      this.budgetService.reopenLastBudget().subscribe(() => {
-        this.messageService.setMessage("Last Budget re-opened.");
-        this.refresh();
+    this.confirmation
+      .ask({
+        title: "Reopen Last Budget",
+        question: "Are you sure you want to open the last closed budget?",
+        emitNegativeAnswers: false
+      })
+      .subscribe(() => {
+        this.messageService.setMessage("Reopening last budget...");
+        this.budgetService.reopenLastBudget().subscribe(() => {
+          this.messageService.setMessage("Last Budget re-opened.");
+          this.refresh();
+        });
       });
-    });
   }
 
   goToBudget(budget: BudgetData) {
@@ -75,34 +79,44 @@ export class BudgetsComponent implements OnInit {
   }
 
   closeBudget(budget: BudgetData) {
-    this.confirmation.ask({
-      title: 'Close Budget Period?',
-      question: 'Are you ready to close off this budget?',
-      emitNegativeAnswers: false
-    }).subscribe(() => {
-      this.messageService.setMessage("Closing....");
-      this.budgetService.closeBudget(budget).subscribe(() => {
-        this.messageService.setMessage("Budget closed.");
-        this.refresh();
-      }, () => {
-        this.messageService.setErrorMessage("Budget NOT closed.");
+    this.confirmation
+      .ask({
+        title: "Close Budget Period?",
+        question: "Are you ready to close off this budget?",
+        emitNegativeAnswers: false
+      })
+      .subscribe(() => {
+        this.messageService.setMessage("Closing....");
+        this.budgetService.closeBudget(budget).subscribe(
+          () => {
+            this.messageService.setMessage("Budget closed.");
+            this.refresh();
+          },
+          () => {
+            this.messageService.setErrorMessage("Budget NOT closed.");
+          }
+        );
       });
-    });
   }
 
   copyBudget(budget: BudgetData) {
-    this.confirmation.ask({
-      title: 'Copy Budget?',
-      question: 'Are you sure you want to COPY this budget?',
-      emitNegativeAnswers: false
-    }).subscribe(() => {
-      this.messageService.setMessage("Copying...");
-      this.budgetService.copyBudget(budget).subscribe(() => {
-        this.messageService.setMessage("Budget copied.");
-        this.refresh();
-      }, () => {
-        this.messageService.setErrorMessage("Budget NOT copied.");
+    this.confirmation
+      .ask({
+        title: "Copy Budget?",
+        question: "Are you sure you want to COPY this budget?",
+        emitNegativeAnswers: false
+      })
+      .subscribe(() => {
+        this.messageService.setMessage("Copying...");
+        this.budgetService.copyBudget(budget).subscribe(
+          () => {
+            this.messageService.setMessage("Budget copied.");
+            this.refresh();
+          },
+          () => {
+            this.messageService.setErrorMessage("Budget NOT copied.");
+          }
+        );
       });
-    });
   }
 }
