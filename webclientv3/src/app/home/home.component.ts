5fa28e87ad1d915afb1aee56c596ec76a95aea76
Kion Stephen <kion.stephen@gmail.com>
2018-06-02 16:41:58 +0200

    implement a display of the date of the last transaction on home page

--- webclientv3/src/app/home/home.component.ts
+++ webclientv3/src/app/home/home.component.ts
@@ -1,32 +1,63 @@
 import { Component, OnInit } from "@angular/core";
 import { MainToolbarService } from "../shared/main-toolbar/main-toolbar.service";
+import { HomeService } from "./home.service";
 
 @Component({
   selector: "ec-home",
   template: `
     <mat-card class="main">
+      <div class="last-update">
+        <h1>Last Transaction Update</h1>
+        <div>
+          <strong>Last transaction entered:</strong>
+          <span class="details">
+            {{ lastUpdate.transaction_date | date:'fullDate' }}
+          </span>
+        </div>
+      </div>
+
       <h1>Recent Updates and Fixes</h1>
       <ul>
-        <li>Implement the Future Budgets view</li>
-        <li>Prevent adding transactions to future budgets</li>
-        <li>Fix issue where the transaction summary shows wrong amount after saving.</li>
+        <li>Indicate that the Setup -> Bank Accounts isn't implemented yet</li>
+        <li>Fix the Account Balances screen for new accounts</li>
       </ul>
 
       <h3>Still outstanding</h3>
       <ul>
-        <li>
-          Move the selection of the import format to part of the configuration of the account.
-          By doing this, selecting an account will select the appropriate import format properly.
-        </li>
+        <li>Implement the Setup of Bank Accounts in the new version</li>
       </ul>
     </mat-card>
   `,
-  styles: []
+  styles: [
+    `
+      .last-update {
+        margin: 15px;
+        background-color: wheat;
+        margin-left: 0;
+        border: 3px solid grey;
+        padding: 15px;
+        border-radius: 10px;
+      }
+
+      .details {
+        border-bottom: 1px solid black;
+      }
+  `
+  ]
 })
 export class HomeComponent implements OnInit {
-  constructor(private mainToolbarService: MainToolbarService) {}
+  public lastUpdate: any = "";
+
+  constructor(
+    private mainToolbarService: MainToolbarService,
+    private homeService: HomeService
+  ) {}
 
   ngOnInit() {
     this.mainToolbarService.showToolbar();
+
+    this.homeService.getLastUpdate().subscribe(lastUpdate => {
+      this.lastUpdate = lastUpdate;
+    });
   }
 }
