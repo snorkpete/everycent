6ee73962df096ebf82d3c4bbddd14cddfa6cd4ba
Kion Stephen <kion.stephen@gmail.com>
2018-07-18 20:01:22 +0200

    Move the transaction list to a Angular Material data table

--- webclientv3/src/app/transactions/transaction-list/transaction-list-header.component.ts
+++ webclientv3/src/app/transactions/transaction-list/transaction-list-header.component.ts
@@ -1,35 +1,19 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {BankAccountData} from "../../bank-accounts/bank-account.model";
+import { Component, Input, OnInit } from "@angular/core";
+import { BankAccountData } from "../../bank-accounts/bank-account.model";
 
-@Component({/* tslint:disable component-selector */
-  selector: '[ec-transaction-list-header]',
+@Component({
+  /* tslint:disable component-selector */
+  selector: "[ec-transaction-list-header]",
   template: `
-    <tr class="heading">
-      <th style="width:5%;"></th>
-      <th style="width:15%;">Date</th>
-      <th style="width:30%;">Description</th>
-
-      <th style="width:20%;" class="allocation-header">
-        <span *ngIf="bankAccount?.is_sink_fund; else allocationHeader">Sink Fund Allocation</span>
-        <ng-template #allocationHeader>
-          <span>Allocation</span>
-        </ng-template>
-      </th>
-      <th style="width:10%;" class="right">Withdrawn</th>
-      <th style="width:10%;" class="right">Deposited</th>
-      <th style="width: 5%;" class="center">Paid?</th>
-      <th style="width: 5%;"></th>
-    </tr>
   `,
-  styles: [`
-  `]
+  styles: [
+    `
+  `
+  ]
 })
 export class TransactionListHeaderComponent implements OnInit {
-
   @Input() bankAccount: BankAccountData;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- webclientv3/src/app/transactions/transaction-list/transaction-list-row.component.ts
+++ webclientv3/src/app/transactions/transaction-list/transaction-list-row.component.ts
@@ -16,58 +16,6 @@ import { TransactionData } from "../transaction-data.model";
   `
   ],
   template: `
-      <td>
-        <mat-checkbox color="primary" [(ngModel)]="transaction.selected"></mat-checkbox>
-      </td>
-      <td>
-        <ec-date-field
-          [editMode]="editMode"
-          [(ngModel)]="transaction.transaction_date"
-          [ecValidateWithinBudget]="budget"
-          [errorMessage]="'test'"
-        >
-        </ec-date-field>
-      </td>
-      <td>
-        <ec-text-field [editMode]="editMode" [(ngModel)]="transaction.description"></ec-text-field>
-      </td>
-
-      <td>
-        <ng-container *ngIf="bankAccount?.is_sink_fund; then sinkFundAllocationField else allocationField">
-        </ng-container>
-
-        <ng-template #sinkFundAllocationField>
-          <ec-list-field [editMode]="editMode"
-                         [items]="sinkFundAllocations"
-                         [(ngModel)]="transaction.sink_fund_allocation_id">
-          </ec-list-field>
-        </ng-template>
-
-        <ng-template #allocationField>
-          <ec-list-field [editMode]="editMode"
-                         [items]="allocations"
-                         (change)="updatePaidStatus($event)"
-                         groupBy="allocation_category"
-                         [(ngModel)]="transaction.allocation_id">
-          </ec-list-field>
-        </ng-template>
-      </td>
-
-      <td class="right">
-        <ec-money-field [editMode]="editMode" [(ngModel)]="transaction.withdrawal_amount"></ec-money-field>
-      </td>
-
-      <td class="right">
-        <ec-money-field [editMode]="editMode" [(ngModel)]="transaction.deposit_amount"></ec-money-field>
-      </td>
-
-      <td class="center">
-        <ec-paid-field [editMode]="editMode" [(ngModel)]="transaction.status"></ec-paid-field>
-      </td>
-
-      <td>
-        <ec-delete-button [editMode]="editMode" [item]="transaction"></ec-delete-button>
-      </td>
   `
 })
 export class TransactionListRowComponent implements OnInit {
@@ -81,13 +29,4 @@ export class TransactionListRowComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
-
-  updatePaidStatus(event) {
-    console.log("fired");
-    if (this.transaction.allocation_id > 0) {
-      this.transaction.status = "paid";
-    } else {
-      this.transaction.status = "unpaid";
-    }
-  }
 }
