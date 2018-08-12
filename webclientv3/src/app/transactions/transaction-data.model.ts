5b994ecfdc76be6e4dd349d4f065dd67c9fb50d4
Kion Stephen <kion.stephen@gmail.com>
2018-08-12 20:14:13 +0200

    reformat by prettier

--- webclientv3/src/app/transactions/transaction-data.model.ts
+++ webclientv3/src/app/transactions/transaction-data.model.ts
@@ -1,12 +1,12 @@
-import {BankAccountData} from "../bank-accounts/bank-account.model";
-import {SinkFundAllocationData} from '../sink-funds/sink-fund-allocation-data.model';
+import { BankAccountData } from "../bank-accounts/bank-account.model";
+import { SinkFundAllocationData } from "../sink-funds/sink-fund-allocation-data.model";
 export interface TransactionData {
   selected?: boolean;
   id?: number;
   description?: string;
   bank_ref?: string;
   bank_account_id?: number;
-  transaction_date?: string|Date;
+  transaction_date?: string | Date;
   withdrawal_amount?: number;
   deposit_amount?: number;
   allocation_id?: number;
