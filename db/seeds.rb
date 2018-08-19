bb37b9077546958034141419924dd2585b780609
Kion Stephen <kion.stephen@gmail.com>
2018-08-19 11:43:47 +0200

    fix the database seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,22 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
 
-# Create the institutions
-Institution.create name:'Scotia Bank'
-Institution.create name:'Unit Trust'
-Institution.create name:'Republic Bank'
-Institution.create name:'RBTT'
-Institution.create name:'First Citizens Bank'
-Institution.create name:'Guardian Asset Management'
+# Create a household using a random ID
+# Seeding this way can then allow for easier migrations later
+household_id = 1 + rand(1000)
+Household.create id: household_id, name: "Household ##{household_id}"
+
+# create default users
+# These users will need further configuration
+User.create(provider: 'email',
+            uid: 'kion.stephen@gmail.com',
+            first_name: 'Kion',
+            last_name: 'Stephen',
+            email: 'kion.stephen@gmail.com',
+            household_id: household_id)
+User.create(provider: 'email',
+            uid: 'patrice.stephen@gmail.com',
+            first_name: 'Patrice',
+            last_name: 'Stephen',
+            email: 'patrice.stephen@gmail.com',
+            household_id: household_id)
