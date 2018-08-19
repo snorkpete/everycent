e021f854c1fa7f00a3d548b1b6832b34e3ed0a60
Kion Stephen <kion.stephen@gmail.com>
2018-08-19 13:22:48 +0200

    make the foreign keys cascading keys

--- db/migrate/20180805223606_add_household_id_everywhere.rb
+++ db/migrate/20180805223606_add_household_id_everywhere.rb
@@ -11,27 +11,27 @@ class AddHouseholdIdEverywhere < ActiveRecord::Migration[5.2]
 
     add_reference :allocation_categories, :household, index: true
     AllocationCategory.update_all household_id: household_id
-    add_foreign_key :allocation_categories, :households
+    add_foreign_key :allocation_categories, :households, on_update: :cascade
 
     add_reference :allocations, :household, index: true
     Allocation.update_all household_id: household_id
-    add_foreign_key :allocations, :households
+    add_foreign_key :allocations, :households, on_update: :cascade
 
     add_reference :bank_accounts, :household, index: true
     BankAccount.update_all household_id: household_id
-    add_foreign_key :bank_accounts, :households
+    add_foreign_key :bank_accounts, :households, on_update: :cascade
 
     add_reference :budgets, :household, index: true
     Budget.update_all household_id: household_id
-    add_foreign_key :budgets, :households
+    add_foreign_key :budgets, :households, on_update: :cascade
 
     add_reference :incomes, :household, index: true
     Income.update_all household_id: household_id
-    add_foreign_key :incomes, :households
+    add_foreign_key :incomes, :households, on_update: :cascade
 
     add_reference :institutions, :household, index: true
     Institution.update_all household_id: household_id
-    add_foreign_key :institutions, :households
+    add_foreign_key :institutions, :households, on_update: :cascade
 
     # add_reference :payees, :household, index: true
     # Payee.update_all household_id: household_id
@@ -47,19 +47,19 @@ class AddHouseholdIdEverywhere < ActiveRecord::Migration[5.2]
 
     add_reference :settings, :household, index: true
     Setting.update_all household_id: household_id
-    add_foreign_key :settings, :households
+    add_foreign_key :settings, :households, on_update: :cascade
 
     add_reference :sink_fund_allocations, :household, index: true
     SinkFundAllocation.update_all household_id: household_id
-    add_foreign_key :sink_fund_allocations, :households
+    add_foreign_key :sink_fund_allocations, :households, on_update: :cascade
 
     add_reference :transactions, :household, index: true
     Transaction.update_all household_id: household_id
-    add_foreign_key :transactions, :households
+    add_foreign_key :transactions, :households, on_update: :cascade
 
     add_reference :users, :household, index: true
     User.update_all household_id: household_id
-    add_foreign_key :users, :households
+    add_foreign_key :users, :households, on_update: :cascade
 
     # also add the admin column
     add_column :users, :admin, :boolean, default: false
