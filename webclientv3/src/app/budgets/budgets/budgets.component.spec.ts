8d7cd2f7fa731ff2f2e2f7d188bd5f1fff7bec69
Kion Stephen <kion.stephen@gmail.com>
2018-09-01 03:03:42 +0200

    prettier reformat

--- webclientv3/src/app/budgets/budgets/budgets.component.spec.ts
+++ webclientv3/src/app/budgets/budgets/budgets.component.spec.ts
@@ -1,31 +1,32 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import {TestConfigModule} from "../../../../test/test-config.module";
-import {SharedModule} from "../../shared/shared.module";
-import {BudgetsModule} from "../budgets.module";
+import { DebugElement } from "@angular/core";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { TestConfigModule } from "../../../../test/test-config.module";
+import { SharedModule } from "../../shared/shared.module";
+import { BudgetsModule } from "../budgets.module";
 
-import { BudgetsComponent } from './budgets.component';
+import { BudgetsComponent } from "./budgets.component";
 
-describe('BudgetsComponent', () => {
+describe("BudgetsComponent", () => {
   let component: BudgetsComponent;
   let fixture: ComponentFixture<BudgetsComponent>;
+  let de: DebugElement;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        TestConfigModule,
-        BudgetsModule,
-        SharedModule.forRoot(),
-      ],
-    });
-  }));
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [TestConfigModule, BudgetsModule, SharedModule.forRoot()]
+      });
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BudgetsComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
