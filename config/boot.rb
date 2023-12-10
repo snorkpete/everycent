1065556b3e0730b7d154a57a283713643dcd7a32
Kion Stephen <kion@kions-mbp.lan>
2023-12-10 10:48:08 +0100

    update to ruby 3.0.3

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,17 @@
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+# ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+#
+# require "bundler/setup" # Set up gems listed in the Gemfile.
+# require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+require_relative "boot"
 
-require "bundler/setup" # Set up gems listed in the Gemfile.
-require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+begin
+  require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+rescue LoadError => e
+  puts e.message
+end
