87633e585797ad76b42b0ee3204e9f131c8bbd11
Kion Stephen <kion.stephen@gmail.com>
2026-03-28 16:59:06 +0100

    production build

--- public/v4/assets/useNotifications-B-igA4UH.js
+++
@@ -1,1 +0,0 @@
-import{b6 as r}from"./index-_xb82NJT.js";function t(){const s=r();return{success(e){s.add({severity:"success",summary:"Success",detail:e,life:3e3})},error(e){s.add({severity:"error",summary:"Error",detail:e,life:5e3})},info(e){s.add({severity:"info",summary:"Info",detail:e,life:3e3})}}}export{t as u};
