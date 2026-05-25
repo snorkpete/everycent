0b887aeab7c3f98718bbb3eac11a77e20f8fe75a
Kion Stephen <kion.stephen@gmail.com>
2026-05-25 10:16:54 +0200

    production build

---
+++ public/assets/index-DnmRHSY3.js
@@ -0,0 +1,142 @@
+import{af as m,o as d,c as p,a as l,aa as r,W as y,aV as g,at as v,$ as C,be as w,aJ as S,N as u,G as I,y as $,g as b,j as A}from"./index-C-rOU8lt.js";var k={name:"MinusIcon",extends:m};function P(e){return T(e)||B(e)||z(e)||_()}function _(){throw new TypeError(`Invalid attempt to spread non-iterable instance.
+In order to be iterable, non-array objects must have a [Symbol.iterator]() method.`)}function z(e,n){if(e){if(typeof e=="string")return h(e,n);var t={}.toString.call(e).slice(8,-1);return t==="Object"&&e.constructor&&(t=e.constructor.name),t==="Map"||t==="Set"?Array.from(e):t==="Arguments"||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t)?h(e,n):void 0}}function B(e){if(typeof Symbol<"u"&&e[Symbol.iterator]!=null||e["@@iterator"]!=null)return Array.from(e)}function T(e){if(Array.isArray(e))return h(e)}function h(e,n){(n==null||n>e.length)&&(n=e.length);for(var t=0,c=Array(n);t<n;t++)c[t]=e[t];return c}function j(e,n,t,c,i,o){return d(),p("svg",r({width:"14",height:"14",viewBox:"0 0 14 14",fill:"none",xmlns:"http://www.w3.org/2000/svg"},e.pti()),P(n[0]||(n[0]=[l("path",{d:"M13.2222 7.77778H0.777778C0.571498 7.77778 0.373667 7.69584 0.227806 7.54998C0.0819442 7.40412 0 7.20629 0 7.00001C0 6.79373 0.0819442 6.5959 0.227806 6.45003C0.373667 6.30417 0.571498 6.22223 0.777778 6.22223H13.2222C13.4285 6.22223 13.6263 6.30417 13.7722 6.45003C13.9181 6.5959 14 6.79373 14 7.00001C14 7.20629 13.9181 7.40412 13.7722 7.54998C13.6263 7.69584 13.4285 7.77778 13.2222 7.77778Z",fill:"currentColor"},null,-1)])),16)}k.render=j;var G=`
+    .p-checkbox {
+        position: relative;
+        display: inline-flex;
+        user-select: none;
+        vertical-align: bottom;
+        width: dt('checkbox.width');
+        height: dt('checkbox.height');
+    }
+
+    .p-checkbox-input {
+        cursor: pointer;
+        appearance: none;
+        position: absolute;
+        inset-block-start: 0;
+        inset-inline-start: 0;
+        width: 100%;
+        height: 100%;
+        padding: 0;
+        margin: 0;
+        opacity: 0;
+        z-index: 1;
+        outline: 0 none;
+        border: 1px solid transparent;
+        border-radius: dt('checkbox.border.radius');
+    }
+
+    .p-checkbox-box {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: dt('checkbox.border.radius');
+        border: 1px solid dt('checkbox.border.color');
+        background: dt('checkbox.background');
+        width: dt('checkbox.width');
+        height: dt('checkbox.height');
+        transition:
+            background dt('checkbox.transition.duration'),
+            color dt('checkbox.transition.duration'),
+            border-color dt('checkbox.transition.duration'),
+            box-shadow dt('checkbox.transition.duration'),
+            outline-color dt('checkbox.transition.duration');
+        outline-color: transparent;
+        box-shadow: dt('checkbox.shadow');
+    }
+
+    .p-checkbox-icon {
+        transition-duration: dt('checkbox.transition.duration');
+        color: dt('checkbox.icon.color');
+        font-size: dt('checkbox.icon.size');
+        width: dt('checkbox.icon.size');
+        height: dt('checkbox.icon.size');
+    }
+
+    .p-checkbox:not(.p-disabled):has(.p-checkbox-input:hover) .p-checkbox-box {
+        border-color: dt('checkbox.hover.border.color');
+    }
+
+    .p-checkbox-checked .p-checkbox-box {
+        border-color: dt('checkbox.checked.border.color');
+        background: dt('checkbox.checked.background');
+    }
+
+    .p-checkbox-checked .p-checkbox-icon {
+        color: dt('checkbox.icon.checked.color');
+    }
+
+    .p-checkbox-checked:not(.p-disabled):has(.p-checkbox-input:hover) .p-checkbox-box {
+        background: dt('checkbox.checked.hover.background');
+        border-color: dt('checkbox.checked.hover.border.color');
+    }
+
+    .p-checkbox-checked:not(.p-disabled):has(.p-checkbox-input:hover) .p-checkbox-icon {
+        color: dt('checkbox.icon.checked.hover.color');
+    }
+
+    .p-checkbox:not(.p-disabled):has(.p-checkbox-input:focus-visible) .p-checkbox-box {
+        border-color: dt('checkbox.focus.border.color');
+        box-shadow: dt('checkbox.focus.ring.shadow');
+        outline: dt('checkbox.focus.ring.width') dt('checkbox.focus.ring.style') dt('checkbox.focus.ring.color');
+        outline-offset: dt('checkbox.focus.ring.offset');
+    }
+
+    .p-checkbox-checked:not(.p-disabled):has(.p-checkbox-input:focus-visible) .p-checkbox-box {
+        border-color: dt('checkbox.checked.focus.border.color');
+    }
+
+    .p-checkbox.p-invalid > .p-checkbox-box {
+        border-color: dt('checkbox.invalid.border.color');
+    }
+
+    .p-checkbox.p-variant-filled .p-checkbox-box {
+        background: dt('checkbox.filled.background');
+    }
+
+    .p-checkbox-checked.p-variant-filled .p-checkbox-box {
+        background: dt('checkbox.checked.background');
+    }
+
+    .p-checkbox-checked.p-variant-filled:not(.p-disabled):has(.p-checkbox-input:hover) .p-checkbox-box {
+        background: dt('checkbox.checked.hover.background');
+    }
+
+    .p-checkbox.p-disabled {
+        opacity: 1;
+    }
+
+    .p-checkbox.p-disabled .p-checkbox-box {
+        background: dt('checkbox.disabled.background');
+        border-color: dt('checkbox.checked.disabled.border.color');
+    }
+
+    .p-checkbox.p-disabled .p-checkbox-box .p-checkbox-icon {
+        color: dt('checkbox.icon.disabled.color');
+    }
+
+    .p-checkbox-sm,
+    .p-checkbox-sm .p-checkbox-box {
+        width: dt('checkbox.sm.width');
+        height: dt('checkbox.sm.height');
+    }
+
+    .p-checkbox-sm .p-checkbox-icon {
+        font-size: dt('checkbox.icon.sm.size');
+        width: dt('checkbox.icon.sm.size');
+        height: dt('checkbox.icon.sm.size');
+    }
+
+    .p-checkbox-lg,
+    .p-checkbox-lg .p-checkbox-box {
+        width: dt('checkbox.lg.width');
+        height: dt('checkbox.lg.height');
+    }
+
+    .p-checkbox-lg .p-checkbox-icon {
+        font-size: dt('checkbox.icon.lg.size');
+        width: dt('checkbox.icon.lg.size');
+        height: dt('checkbox.icon.lg.size');
+    }
+`,O={root:function(n){var t=n.instance,c=n.props;return["p-checkbox p-component",{"p-checkbox-checked":t.checked,"p-disabled":c.disabled,"p-invalid":t.$pcCheckboxGroup?t.$pcCheckboxGroup.$invalid:t.$invalid,"p-variant-filled":t.$variant==="filled","p-checkbox-sm p-inputfield-sm":c.size==="small","p-checkbox-lg p-inputfield-lg":c.size==="large"}]},box:"p-checkbox-box",input:"p-checkbox-input",icon:"p-checkbox-icon"},N=y.extend({name:"checkbox",style:G,classes:O}),F={name:"BaseCheckbox",extends:v,props:{value:null,binary:Boolean,indeterminate:{type:Boolean,default:!1},trueValue:{type:null,default:!0},falseValue:{type:null,default:!1},readonly:{type:Boolean,default:!1},required:{type:Boolean,default:!1},tabindex:{type:Number,default:null},inputId:{type:String,default:null},inputClass:{type:[String,Object],default:null},inputStyle:{type:Object,default:null},ariaLabelledby:{type:String,default:null},ariaLabel:{type:String,default:null}},style:N,provide:function(){return{$pcCheckbox:this,$parentInstance:this}}};function a(e){"@babel/helpers - typeof";return a=typeof Symbol=="function"&&typeof Symbol.iterator=="symbol"?function(n){return typeof n}:function(n){return n&&typeof Symbol=="function"&&n.constructor===Symbol&&n!==Symbol.prototype?"symbol":typeof n},a(e)}function V(e,n,t){return(n=L(n))in e?Object.defineProperty(e,n,{value:t,enumerable:!0,configurable:!0,writable:!0}):e[n]=t,e}function L(e){var n=q(e,"string");return a(n)=="symbol"?n:n+""}function q(e,n){if(a(e)!="object"||!e)return e;var t=e[Symbol.toPrimitive];if(t!==void 0){var c=t.call(e,n);if(a(c)!="object")return c;throw new TypeError("@@toPrimitive must return a primitive value.")}return(n==="string"?String:Number)(e)}function E(e){return U(e)||W(e)||M(e)||H()}function H(){throw new TypeError(`Invalid attempt to spread non-iterable instance.
+In order to be iterable, non-array objects must have a [Symbol.iterator]() method.`)}function M(e,n){if(e){if(typeof e=="string")return s(e,n);var t={}.toString.call(e).slice(8,-1);return t==="Object"&&e.constructor&&(t=e.constructor.name),t==="Map"||t==="Set"?Array.from(e):t==="Arguments"||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t)?s(e,n):void 0}}function W(e){if(typeof Symbol<"u"&&e[Symbol.iterator]!=null||e["@@iterator"]!=null)return Array.from(e)}function U(e){if(Array.isArray(e))return s(e)}function s(e,n){(n==null||n>e.length)&&(n=e.length);for(var t=0,c=Array(n);t<n;t++)c[t]=e[t];return c}var J={name:"Checkbox",extends:F,inheritAttrs:!1,emits:["change","focus","blur","update:indeterminate"],inject:{$pcCheckboxGroup:{default:void 0}},data:function(){return{d_indeterminate:this.indeterminate}},watch:{indeterminate:function(n){this.d_indeterminate=n,this.updateIndeterminate()}},mounted:function(){this.updateIndeterminate()},updated:function(){this.updateIndeterminate()},methods:{getPTOptions:function(n){var t=n==="root"?this.ptmi:this.ptm;return t(n,{context:{checked:this.checked,indeterminate:this.d_indeterminate,disabled:this.disabled}})},onChange:function(n){var t=this;if(!this.disabled&&!this.readonly){var c=this.$pcCheckboxGroup?this.$pcCheckboxGroup.d_value:this.d_value,i;this.binary?i=this.d_indeterminate?this.trueValue:this.checked?this.falseValue:this.trueValue:this.checked||this.d_indeterminate?i=c.filter(function(o){return!S(o,t.value)}):i=c?[].concat(E(c),[this.value]):[this.value],this.d_indeterminate&&(this.d_indeterminate=!1,this.$emit("update:indeterminate",this.d_indeterminate)),this.$pcCheckboxGroup?this.$pcCheckboxGroup.writeValue(i,n):this.writeValue(i,n),this.$emit("change",n)}},onFocus:function(n){this.$emit("focus",n)},onBlur:function(n){var t,c;this.$emit("blur",n),(t=(c=this.formField).onBlur)===null||t===void 0||t.call(c,n)},updateIndeterminate:function(){this.$refs.input&&(this.$refs.input.indeterminate=this.d_indeterminate)}},computed:{groupName:function(){return this.$pcCheckboxGroup?this.$pcCheckboxGroup.groupName:this.$formName},checked:function(){var n=this.$pcCheckboxGroup?this.$pcCheckboxGroup.d_value:this.d_value;return this.d_indeterminate?!1:this.binary?n===this.trueValue:w(this.value,n)},dataP:function(){return C(V({invalid:this.$invalid,checked:this.checked,disabled:this.disabled,filled:this.$variant==="filled"},this.size,this.size))}},components:{CheckIcon:g,MinusIcon:k}},K=["data-p-checked","data-p-indeterminate","data-p-disabled","data-p"],Z=["id","value","name","checked","tabindex","disabled","readonly","required","aria-labelledby","aria-label","aria-invalid"],D=["data-p"];function Q(e,n,t,c,i,o){var x=u("CheckIcon"),f=u("MinusIcon");return d(),p("div",r({class:e.cx("root")},o.getPTOptions("root"),{"data-p-checked":o.checked,"data-p-indeterminate":i.d_indeterminate||void 0,"data-p-disabled":e.disabled,"data-p":o.dataP}),[l("input",r({ref:"input",id:e.inputId,type:"checkbox",class:[e.cx("input"),e.inputClass],style:e.inputStyle,value:e.value,name:o.groupName,checked:o.checked,tabindex:e.tabindex,disabled:e.disabled,readonly:e.readonly,required:e.required,"aria-labelledby":e.ariaLabelledby,"aria-label":e.ariaLabel,"aria-invalid":e.invalid||void 0,onFocus:n[0]||(n[0]=function(){return o.onFocus&&o.onFocus.apply(o,arguments)}),onBlur:n[1]||(n[1]=function(){return o.onBlur&&o.onBlur.apply(o,arguments)}),onChange:n[2]||(n[2]=function(){return o.onChange&&o.onChange.apply(o,arguments)})},o.getPTOptions("input")),null,16,Z),l("div",r({class:e.cx("box")},o.getPTOptions("box"),{"data-p":o.dataP}),[I(e.$slots,"icon",{checked:o.checked,indeterminate:i.d_indeterminate,class:$(e.cx("icon")),dataP:o.dataP},function(){return[o.checked?(d(),b(x,r({key:0,class:e.cx("icon")},o.getPTOptions("icon"),{"data-p":o.dataP}),null,16,["class","data-p"])):i.d_indeterminate?(d(),b(f,r({key:1,class:e.cx("icon")},o.getPTOptions("icon"),{"data-p":o.dataP}),null,16,["class","data-p"])):A("",!0)]})],16,D)],16,K)}J.render=Q;export{J as s};
---
+++ public/assets/useReadyStatus-g_NaJMe1.js
@@ -0,0 +1,1 @@
+import{q as r}from"./index-C-rOU8lt.js";function a(e){return r(()=>!e.loading.value&&!e.error.value)}export{a as u};
